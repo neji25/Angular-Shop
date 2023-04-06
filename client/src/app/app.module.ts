d517f9c381154fa370291878e1854aa7682986e1
Vlad <myborvlad@gmail.com>
2023-04-06 23:47:02 +0300

    Create pages

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './app/pages/admin/admin.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { BasketComponent } from './pages/basket/basket.component';
+import { DevicePageComponent } from './pages/device-page/device-page.component';
+import { ShopComponent } from './pages/shop/shop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    AuthComponent,
+    BasketComponent,
+    DevicePageComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
