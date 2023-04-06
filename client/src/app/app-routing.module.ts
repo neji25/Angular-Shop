f1f18f8d7431e60847c4919a7aab744c58ebc8e7
Vlad <myborvlad@gmail.com>
2023-04-07 00:07:54 +0300

    Some changes in routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminComponent} from "./pages/admin/admin.component";
+import {AuthComponent} from "./pages/auth/auth.component";
+import {ShopComponent} from "./pages/shop/shop.component";
+import {BasketComponent} from "./pages/basket/basket.component";
+import {DevicePageComponent} from "./pages/device-page/device-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminComponent},
+  {path: 'login', component: AuthComponent},
+  {path: 'registration', component: AuthComponent},
+  {path: '', component: ShopComponent},
+  {path: 'basket', component: BasketComponent},
+  {path: 'device', component: DevicePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor() {
+  }
+
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './app/pages/admin/admin.component';
+import { AdminComponent } from './pages/admin/admin.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { BasketComponent } from './pages/basket/basket.component';
 import { DevicePageComponent } from './pages/device-page/device-page.component';
