8127a6a2709cb11b54e22fd386336744ec74e37d
Vlad <myborvlad@gmail.com>
2023-05-30 08:46:59 +0300

    Added CanActivate

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import {ShopComponent} from "./pages/shop/shop.component";
 import {BasketComponent} from "./pages/basket/basket.component";
 import {DevicePageComponent} from "./pages/device-page/device-page.component";
 import {ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "./consts.module";
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
-  {path: ADMIN_ROUTE, component: AdminComponent},
+  {path: ADMIN_ROUTE, component: AdminComponent, canActivate: [AuthGuard]},
   {path: LOGIN_ROUTE, component: AuthComponent},
   {path: REGISTRATION_ROUTE, component: AuthComponent},
   {path: SHOP_ROUTE, component: ShopComponent},
-  {path: BASKET_ROUTE, component: BasketComponent},
+  {path: BASKET_ROUTE, component: BasketComponent, canActivate: [AuthGuard]},
   {path: DEVICE_ROUTE + '/:id', component: DevicePageComponent},
 ];
 
---
+++ client/src/app/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import {inject, Injectable} from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  authService = inject(AuthService)
+  canActivate(n
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isLoggedIn;
+  }
+
+}
