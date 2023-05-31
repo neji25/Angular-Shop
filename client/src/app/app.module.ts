f729d1550b6b4021b5e47160c19b538ee1fccc58
Borodiuk Vlad <myborvlad@gmail.com>
2023-05-31 16:41:03 +0300

    Added material navBar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { ShopComponent } from './pages/shop/shop.component';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +28,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NavBarComponent,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/nav-bar/nav-bar.component.ts
+++ client/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,22 +1,17 @@
 import {Component, ViewChild} from '@angular/core';
-import {MatMenuModule, MatMenuTrigger} from "@angular/material/menu";
-import {MatIconModule} from "@angular/material/icon";
+import {MatMenuTrigger} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
   templateUrl: 'nav-bar.component.html',
   imports: [
-    MatMenuModule,
-    MatIconModule,
+    MatToolbarModule,
     MatButtonModule
   ]
 })
 export class NavBarComponent {
-  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger | any
-
-  someMethod() {
-    this.trigger.openMenu();
-  }
+  
 }
